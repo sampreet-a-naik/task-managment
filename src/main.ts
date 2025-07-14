31ea2337a723e96712bc98ceb98db46b8ad20d31
Sampreet Naik <sampreetnaik@Mac.lan>
2025-07-13 19:34:27 -0500

    added tasks

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
-import { App } from './app/app';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(App).catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
 
