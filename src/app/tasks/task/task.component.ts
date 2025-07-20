37de580fd574a05ea89449825dc49457fec7ec43
Sampreet Naik <sampreetnaik@Mac.lan>
2025-07-20 00:39:25 -0500

    add new task

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { type Task } from "./task.modal";
 
 @Component({
-  selector: 'app-task',
+  selector: "app-task",
   standalone: true,
   imports: [],
-  templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  templateUrl: "./task.component.html",
+  styleUrl: "./task.component.css",
 })
 export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+  @Output() complete = new EventEmitter<string>();
 
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,20 @@
-import { Component, Input, Output, EventEmitter  } from '@angular/core';
-
-interface User {
-    id: string;
-    avatar: string;
-    name: string; 
-  }
-
-
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { type User } from "./user.modal";
 @Component({
-  selector: 'app-user',
+  selector: "app-user",
   standalone: true,
   imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  templateUrl: "./user.component.html",
+  styleUrl: "./user.component.css",
 })
 export class UserComponent {
-  @Input({ required : true }) user! : User;
-  @Output() select = new EventEmitter();
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
   get imagePath() {
-    return 'assets/users/' + this.user.avatar;
+    return "assets/users/" + this.user.avatar;
   }
-   onSelectUser () {
-    this.select.emit(this.user.id)
-   }
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
 }
-
